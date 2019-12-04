20981b6233efde88b7099a79af864c024fc8628d
Saniyat Al Ahmed <32844821+saniyat013@users.noreply.github.com>
2019-12-04 13:22:38 +0600

    Updated Stack Implementation

--- Stack.java
+++ Stack.java
@@ -1,89 +1,147 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-
-public class Stack {
-    static int stack[];
-    static int top = -1;
+class StackModel{
+    int stack[];
+    int top;
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of the Stack:");
-        int size = sc.nextInt();
-        stack = new int[size];
+    StackModel(int n){
+        stack = new int[n];
+        top = -1;
         Arrays.fill(stack, -1);
-        
-        push(stack, 5);
-        push(stack, 10);
-        push(stack, 15);
-        push(stack, 20);
-        System.out.println("Max in Stack: " + max(stack));
-        System.out.println("Min in Stack: " + min(stack));
-        System.out.println("Size of Stack: " + size(stack));
-        System.out.println("Number of Elements in Stack: " + count(stack));
-        System.out.println("Top of Stack: " + top(stack));
-        System.out.println("Element Popped: " + pop(stack));
-        System.out.println("Top of Stack: " + top(stack));
-        System.out.println("Max in Stack: " + max(stack));
-        System.out.println("Min in Stack: " + min(stack));
     }
     
-    static void push(int stack[], int n){
-        if(top >= stack.length)
-            System.out.println("Stack is Full");
-        else
-            stack[++top] = n;
+    int push(int n){
+        if(this.top + 1 >= this.stack.length){
+            return -1;
+        }else{
+            this.stack[++top] = n;
+            return 0;
+        }     
     }
     
-    static int pop(int stack[]){
+    void pop(){
         if(top < 0){
             System.out.println("Stack is Empty");
-            return -1;
         }else{
             int temp = stack[top];
             stack[top] = -1;
             top--;
-            return temp;
+            System.out.println("Popped item: " + temp);
         }            
     }
     
-    static int top(int stack[]){
+    void top(){
         if(top < 0){
             System.out.println("Stack is Empty");
-            return -1;
         }else{
-            return stack[top];
+            System.out.println("Top of the Stack: " + stack[top]);
         }  
     }
     
-    static int max(int stack[]){
+    void max(){
         int max = stack[0];
         for(int x: stack){
             if(max < x)
                 max = x;
         }
-        return max;
+        System.out.println("MAX item in the Stack: " + max);
     }
     
-    static int min(int stack[]){
+    void min(){
         int min = stack[0];
         for(int x: stack){
             if(min > x && x > -1)
                 min = x;
         }
-        return min;
+        System.out.println("MIN item in the Stack: " + min);
     }
     
-    static int size(int stack[]){
-        return stack.length;
+    void size(){
+        System.out.println("Size of the Stack: " + stack.length);
     }
     
-    static int count(int stack[]){
-        int i = 0, count = 0;
-        while(stack[i++] >= 0){
-            count++;
+    void count(){
+        System.out.println(this.top);
+        int count = 0;
+        for (int i = 0; i < this.stack.length; i++) {
+            if(this.stack[i] >= 0)
+                count++;
+            else
+                break;
         }
-        return count;
+        System.out.println("Number of items in the Stack: " + count);
     }
+}
+
+public class Stack {   
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i, j, k, n, result;
+        
+        do{
+            System.out.println("Enter the size of the Stack: ");
+            j = sc.nextInt();
+            StackModel stack = new StackModel(j);
+            
+            do{
+                System.out.println("Implementation of Stack. Enter your choice:"
+                + "\n1. Push item into Stack"
+                + "\n2. Pop an item"
+                + "\n3. View top item of the Stack"
+                + "\n4. Find the MAX item"
+                + "\n5. Find the MIN item"
+                + "\n6. Size of the Stack"
+                + "\n7. Number of elements of the Stack"
+                + "\n0. Exit");
+                i = sc.nextInt();
+
+                switch(i){
+                    case 1:{
+                        System.out.println("Enter item to insert: ");
+                        n = sc.nextInt();
+                        result = stack.push(n);
+                        if(result == -1){
+                            System.out.println("Stack is FULL. Can't Insert");
+                        }else{
+                            System.out.println(n + " inserted to the Stack");
+                        }
+                        break;
+                    }
+                    case 2:{
+                        stack.pop();
+                        break;
+                    }
+                    case 3:{
+                        stack.top();
+                        break;
+                    }
+                    case 4:{
+                        stack.max();
+                        break;
+                    }
+                    case 5:{
+                        stack.min();
+                        break;
+                    }
+                    case 6:{
+                        stack.size();
+                        break;
+                    }
+                    case 7:{
+                        stack.count();
+                        break;
+                    }default:{
+                        System.out.println("Please select a valid number.");
+                    }
+                }
+            }while(i != 0);
+            
+            System.out.println("Want to create another Stack?"
+                + "\n1. YES"
+                + "\n2. NO");
+            k = sc.nextInt();
+            
+        }while(k != 2);     
+    }   
 }
